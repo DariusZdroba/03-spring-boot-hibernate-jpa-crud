0b07b3b0c901bd13b37d295a0aa0916e622f0940
Darius <dariuszdroba@gmail.com>
2024-03-10 15:27:01 +0200

    'initialCommit'

---
+++ 64 - 00-starter-sql-scripts/00-starter-sql-scripts/01-create-user.sql
@@ -0,0 +1,8 @@
+
+-- Drop user if they exist
+DROP USER if exists 'springstudent'@'localhost';
+
+-- Now create user with prop privileges
+CREATE USER 'springstudent'@'%' IDENTIFIED BY 'springstudent';
+
+GRANT ALL PRIVILEGES ON * . * TO 'springstudent'@'%';
